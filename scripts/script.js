d3d2ac8fe5168916a1468da9655e63382242bf23
Maybegin10 <148011653+Maybegin10@users.noreply.github.com>
2023-11-22 14:55:10 -0600

    cp12

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,10 @@
 /*
-Author: Eric Davis
-Date: 11/7/2023
-File Name: script.js
+    Student Name: Eric Davis
+    File Name: script.js
+    Date: 11/22/2023
 */
 
-//Global variables
-var video = document.getElementById("example");
-var videoSource = document.getElementById("vid-src");
-var descriptionSource = document.getElementById("despsrc");
-
-//Hamburger menu function
-function hamburger() {
-	var menu = document.getElementById("menu-links");
-	var logo = document.getElementById("ffc-logo");
-	if (menu.style.display === "block" && logo.style.display === "none") {
-		menu.style.display = "none";
-		logo.style.display = "block";
-	} else {
-		menu.style.display = "block";
-		logo.style.display = "none";
-	}
-}
-
-//Function to display the burpees example video
-function burpees() {
-	videoSource.src = "media/burpees.mp4";
-	descriptionSource.src = "media/burpees-descriptions.vtt";
-	video.style.display = "block";
-	video.load();
-}
-
-//Function to display the plank example video 
-function plank() {
-	videoSource.src = "media/plank.mp4";
-	descriptionSource.src = "media/plank-descriptions.vtt";
-	video.style.display = "block";
-	video.load();
-}
-
-//Function to display the mountain climbers example video 
-function mountain() {
-	videoSource.src = "media/mc.mp4";
-	 descriptionSource.src = "media/mountain-descriptions.vtt";
-	 video.style.display = "block";
-	 video.load();
-}
-
-//Function to display a promo code
-function discount() {
-	var promo = document.getElementById("special");
-	promo.firstChild.nodeValue = "Promo Code: D25START";
-	promo.style.color = "#ff0000";
-	promo.style.fontSize = "2em";
-}
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+	$('.hero').height($(window).height());
+});
